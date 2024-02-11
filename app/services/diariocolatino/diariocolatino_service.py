743682f8d9716deb89cb44c047cb7b9b937d72ba
RauPro <00012119@uca.edu.sv>
2024-02-11 14:19:59 -0500

    Add: Scrapper Diario Colatino

---
+++ app/services/diariocolatino/diariocolatino_service.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class ElSalvadorScraper:
+    def __init__(self, query='Feminicidio'):
+        self.base_url = 'https://www.diariocolatino.com/?s=' + query
+
+    def init_search_urls(self):
+        response = requests.get(self.base_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            articles = soup.find_all('article', class_='item-list')
+            list_urls = []
+            for article in articles:
+                link = article.find('a', class_='more-link')
+                list_urls.append(link['href'])
+            return list_urls
+
+    def get_url_content(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            h1 = soup.find('h1', class_='name post-title entry-title')
+            h1 = h1 if h1.find('span') else None
+            article = soup.find('div', class_='entry')
+            paragraphs = article.find_all('p') if article else []
+            news_text = ' '.join(paragraph.text for paragraph in paragraphs)
+            new = {
+                'title': h1.text if h1 else 'No se encontró el título',
+                'text': news_text}
+            return new
+if __name__ == '__main__':
+    cl = ElSalvadorScraper()
+    urls = cl.init_search_urls()
+    for url in urls:
+        print(cl.get_url_content(url))
