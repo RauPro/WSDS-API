48a5b0f79dc388ec88e943a1425ba10a768604fd
Emely <00098619@uca.edu.sv>
2024-04-18 21:10:07 -0600

    Fix Issue Cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
 
-from app.controllers.notices_controller import test_create_notice
-from services.diariocolatino import DiarioColatinoScrapper
-from services.elsalvador import ElSalvadorScraper
-from services.diarioelmundo import DiarioElMundoScrapper
-from services.diarioelsalvador import DiarioElSalvadorScrapper
-from services.driver import classify_new
-from env import env
+##from app.controllers.notices_controller import test_create_notice
+from .services.diariocolatino import DiarioColatinoScrapper
+from .services.elsalvador import ElSalvadorScraper
+from .services.diarioelmundo import DiarioElMundoScrapper
+from .services.diarioelsalvador import DiarioElSalvadorScrapper
+from .services.driver import classify_new
+from .env import env
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 env.set_env()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:4200"
+]
+
+app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/poc")
 async def poc(search: str = ""):
