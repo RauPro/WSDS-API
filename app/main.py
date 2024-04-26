8924bd4a62d1417191ca2d9d8252804cbd0bd708
RauPro <00012119@uca.edu.sv>
2024-04-25 18:19:55 -0600

    Add optm

--- app/main.py
+++ app/main.py
@@ -1,107 +1,115 @@
+# Importing necessary libraries for web application setup
+import random
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+# Importing specific scrapping services
 from app.controllers.notices_controller import test_create_notice
-from services.diariocolatino import DiarioColatinoScrapper
-from services.elsalvador import ElSalvadorScraper
-from services.diarioelmundo import DiarioElMundoScrapper
-from services.diarioelsalvador import DiarioElSalvadorScrapper
-from services.driver import classify_new
-from env import env
+from .services.diariocolatino import DiarioColatinoScrapper
+from .services.elsalvador import ElSalvadorScraper
+from .services.diarioelmundo import DiarioElMundoScrapper
+from .services.diarioelsalvador import DiarioElSalvadorScrapper
+from .env import env
 
+# Initializing FastAPI application
 app = FastAPI()
 
+# Setting up environment variables
 env.set_env()
 
+# Defining origins for CORS
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:4200"
+]
+
+# Configuring CORS middleware
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
+
+
+# Helper function to perform scraping
+async def perform_scraping(scraper):
+    urls = scraper.init_search_urls()
+    content_urls = [scraper.get_url_content(url) for url in urls]
+    return content_urls
+
+
+# Route for proof of concept using ElSalvadorScraper
 @app.get("/poc")
 async def poc(search: str = ""):
     scraper = ElSalvadorScraper(search)
-    urls = scraper.init_search_urls()
-    content_urls = []
-    for url in urls:
-        content_urls.append(scraper.get_url_content(url))
-    return content_urls
+    return await perform_scraping(scraper)
 
+
+# Route for Diario Colatino scraper
 @app.get("/colatino")
 async def colatino(search: str = ""):
     scraper = DiarioColatinoScrapper(search)
-    urls = scraper.init_search_urls()
-    content_urls = []
-    for url in urls:
-        content_urls.append(scraper.get_url_content(url))
-    return content_urls
+    return await perform_scraping(scraper)
 
+
+# Route for Diario El Mundo scraper
 @app.get("/diarioelmundo")
 async def diarioelmundo(search: str = ""):
     scraper = DiarioElMundoScrapper(search)
-    urls = scraper.init_search_urls()
-    content_urls = []
-    for url in urls:
-        content_urls.append(scraper.get_url_content(url))
-    return content_urls
+    return await perform_scraping(scraper)
 
 
+# Route for Diario El Salvador scraper
 @app.get("/diarioelsalvador")
 async def diarioelsalvador(search: str = "Feminicidio"):
     scraper = DiarioElSalvadorScrapper(search)
-    urls = scraper.init_search_urls()
-    content_urls = []
-    for url in urls:
-        content_urls.append(scraper.get_url_content(url))
-    return content_urls
+    return await perform_scraping(scraper)
 
+
+# Route for global search across multiple sources
 @app.get("/global")
 async def global_search(search: str = "Feminicidio"):
-    scraperDiarioElSalvadorScrapper = DiarioElSalvadorScrapper(search)
-    scraperDiarioColatinoScrapper = DiarioColatinoScrapper(search)
-    scraperDiarioElMundoScrapper = DiarioElMundoScrapper(search)
-    urlsDiarioElSalvadorScrapper = scraperDiarioElSalvadorScrapper.init_search_urls()
+    scrapers = [
+        DiarioElSalvadorScrapper(search),
+        DiarioColatinoScrapper(search),
+        DiarioElMundoScrapper(search)
+    ]
     content_urls = []
-    for url in urlsDiarioElSalvadorScrapper:
-        content_urls.append(scraperDiarioElSalvadorScrapper.get_url_content(url))
-    print(len(content_urls))
-    urlsDiarioColatinoScrapper = scraperDiarioColatinoScrapper.init_search_urls()
-    #content_urls = []
-    for url in urlsDiarioColatinoScrapper:
-        content_urls.append(scraperDiarioColatinoScrapper.get_url_content(url))
-    print(len(content_urls))
-    urlsDiarioElMundoScrapper = scraperDiarioElMundoScrapper.init_search_urls()
-    #content_urls = []
-    for url in urlsDiarioElMundoScrapper:
-        content_urls.append(scraperDiarioElMundoScrapper.get_url_content(url))
-    print(len(content_urls))
-    for i in range(len(content_urls)):
-        content_urls[i]['tag'] = classify_new(content_urls[i]['text'])
-    print(len(content_urls))
+    for scraper in scrapers:
+        scraper_urls = await perform_scraping(scraper)
+        for url_content in scraper_urls:
+            if url_content != None:
+                url_content['tag'] = search
+        content_urls.extend(scraper_urls)
+    content_urls = [url for url in content_urls if url is not None]
+    random.shuffle(content_urls)
     return content_urls
 
 
+# Simplified static global search for demonstration purposes
 def global_search_static(search: str = "Feminicidio"):
-    scraperDiarioElSalvadorScrapper = DiarioElSalvadorScrapper(search)
-    scraperDiarioColatinoScrapper = DiarioColatinoScrapper(search)
-    scraperDiarioElMundoScrapper = DiarioElMundoScrapper(search)
-    urlsDiarioElSalvadorScrapper = scraperDiarioElSalvadorScrapper.init_search_urls()
+    scrapers = [
+        DiarioElSalvadorScrapper(search),
+        DiarioColatinoScrapper(search),
+        DiarioElMundoScrapper(search)
+    ]
     content_urls = []
-    for url in urlsDiarioElSalvadorScrapper:
-        content_urls.append(scraperDiarioElSalvadorScrapper.get_url_content(url))
-    print(len(content_urls))
-    urlsDiarioColatinoScrapper = scraperDiarioColatinoScrapper.init_search_urls()
-    #content_urls = []
-    for url in urlsDiarioColatinoScrapper:
-        content_urls.append(scraperDiarioColatinoScrapper.get_url_content(url))
-    print(len(content_urls))
-    urlsDiarioElMundoScrapper = scraperDiarioElMundoScrapper.init_search_urls()
-    #content_urls = []
-    for url in urlsDiarioElMundoScrapper:
-        content_urls.append(scraperDiarioElMundoScrapper.get_url_content(url))
-    print(len(content_urls))
-    for i in range(len(content_urls)):
-        content_urls[i]['tag'] = classify_new(content_urls[i]['text'])
-    print(len(content_urls))
+    for scraper in scrapers:
+        scraper_urls = [scraper.get_url_content(url) for url in scraper.init_search_urls()]
+        for url_content in scraper_urls:
+            url_content['tag'] = search
+        content_urls.extend(scraper_urls)
     return content_urls
+
+
+# Route to integrate with a hypothetical model for creating notices
 @app.get("/model_gemma")
 async def model_gemma(search: str = "Feminicidio"):
     return test_create_notice(global_search_static())
 
+
+# Entry point when run as a main program
 if __name__ == '__main__':
     print("TEST JOIN")
-    #test_create_notice()
