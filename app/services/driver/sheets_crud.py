6ec520f3cbc38c6dc952d0463713c4071a14eacf
RauPro <00012119@uca.edu.sv>
2024-05-12 16:01:03 -0600

    Create sheets

--- app/services/driver/sheets_crud.py
+++ app/services/driver/sheets_crud.py
@@ -6,7 +6,12 @@ collection = DatabaseService.get_sheets_collection()
 
 
 def create_sheet(sheet_data: SheetEntry):
-    collection.insert_one(sheet_data.dict())
+    try:
+        collection.insert_one(sheet_data)
+    except Exception as e:
+        # Log the exception or handle it accordingly
+        print(f"Failed to create sheet: {e}")
+        raise
 
 
 def get_sheets():
@@ -19,14 +24,43 @@ def get_sheet_by_id(sheet_id: str):
 
 
 def update_sheet(sheet_id: str, sheet_data: SheetEntry):
-    collection.update_one({"id": sheet_id}, {"$set": sheet_data.dict()})
+    collection.update_one({"id": sheet_id}, {"$set": sheet_data})
 
 
 def delete_sheet(sheet_id: str):
     collection.delete_one({"id": sheet_id})
 
+
 def create_or_update_sheet(sheet: SheetEntry, existing_sheet):
     if existing_sheet is None:
         create_sheet(sheet)
     else:
-        update_sheet(sheet.id, sheet)
+        update_sheet(sheet["id"], sheet)
+
+
+def create_sheet_priority(sheet: SheetEntry):
+    existing_sheet = get_sheet_by_id(sheet["id"])
+    if sheet["priority"] == 1:
+        create_or_update_sheet(sheet, existing_sheet)
+        return({"message": "Sheet created/updated successfully with priority 1", "data": sheet})
+
+    elif sheet["priority"] == 2:
+        if existing_sheet is None or existing_sheet.get('priority', 999) < 2:
+            create_or_update_sheet(sheet, existing_sheet)
+            return({"message": "Sheet created/updated successfully with priority 2", "data": sheet})
+        else:
+            return({"message": "Cannot overwrite existing sheet with higher priority"})
+
+    elif sheet["priority"] == 3:
+        if (existing_sheet is not None and existing_sheet.get('priority', 999) == 4) or existing_sheet is None:
+            create_or_update_sheet(sheet, existing_sheet)
+            return({"message": "Sheet updated successfully with priority 3", "data": sheet})
+        else:
+            return({"message": "Cannot overwrite existing sheet unless priority is 4"})
+
+    elif sheet["priority"] > 3:
+        create_or_update_sheet(sheet, existing_sheet)
+        return({"message": "Sheet created/updated successfully", "data": sheet})
+
+    else:
+        return({"message": "Sorry, we can't save this sheet due to priority rules"})
